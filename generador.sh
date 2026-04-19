37465785cf2c19e2dbef9062f0169212fe1ac482
Agustin Flores Lopez <afl231001@gmail.com>
2026-04-18 20:09:12 -0600

    Intercambia correctamente los archivos de configuracion

---
+++ generador.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mv configuracionAnterior.json ~/.config/kanshi/config.tmp
+mv ~/.config/kanshi/config configuracionAnterior.json
+mv ~/.config/kanshi/config.tmp ~/.config/kanshi/config 		
