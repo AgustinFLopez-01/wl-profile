9c53fa9565d7f7fe0bc9704d44ff4a509af4310e
Agustin Flores Lopez <afl231001@gmail.com>
2026-04-18 20:19:11 -0600

    Se arreglo un error de tipeo, además se agrego una automatización para refrescar la vista al hacer el cambio

--- generador.sh
+++ generador.sh
@@ -2,3 +2,5 @@
 mv configuracionAnterior.json ~/.config/kanshi/config.tmp
 mv ~/.config/kanshi/config configuracionAnterior.json
 mv ~/.config/kanshi/config.tmp ~/.config/kanshi/config 		
+pkill kanshi
+kanshi &
